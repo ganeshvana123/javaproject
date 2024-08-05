ca75a0d133e9091306885010c8dcc8a6131337f4
ganeshvana123 <ganeshvana955@gmail.com>
2024-08-05 22:01:40 +0530

    added new line

--- hello.java
+++ hello.java
@@ -4,5 +4,6 @@ class hello{
         System.out.println("this is the first change in the github value");
         System.out.println("new line is added to the this hello folder");
         System.out.println("here i am added new line to hello file ");
+        System.out.println("checking the git status");
     }
 }
