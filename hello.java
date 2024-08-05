39b965c2734b2fb59d0b81db20d6be74822f16df
ganeshvana123 <ganeshvana955@gmail.com>
2024-08-05 21:48:22 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ class hello{
         System.out.println("this is the first git hub desktop file");
         System.out.println("this is the first change in the github value");
         System.out.println("new line is added to the this hello folder");
+        System.out.println("here i am added new line to hello file ");
     }
 }
