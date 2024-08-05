f6fd47a2b5189a13e32b9ae8fb1477f1a5da8069
ganeshvana123 <ganeshvana955@gmail.com>
2024-08-05 21:44:45 +0530

    hello.java is changed

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class hello{
     public static void main(String[] args) {
         System.out.println("this is the first git hub desktop file");
         System.out.println("this is the first change in the github value");
+        System.out.println("new line is added to the this hello folder");
     }
 }
